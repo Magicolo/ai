5e45a99ed8a8360925d58a8194d24ec231d64bbd
Magicolo <magicololand@gmail.com>
2024-09-13 01:32:41 -0400

    no more quiet

--- ollama/serve.sh
+++ ollama/serve.sh
@@ -3,8 +3,8 @@
 port="${1:-11434}"
 folder="$(realpath $(dirname $0))"
 
-docker volume create ollama &> /dev/null || exit $?
-docker compose --file "$folder/docker-compose.yml" build --quiet ollama &> /dev/null || exit $?
+docker volume create ollama || exit $?
+docker compose --file "$folder/docker-compose.yml" build ollama || exit $?
 if [ "$port" -le 0 ]; then
     docker compose --file "$folder/docker-compose.yml" run --rm --detach ollama
 else
