a280a95559440ddb7ba9dc984be9c6ad16685a67
Magicolo <magicololand@gmail.com>
2024-09-18 00:30:59 +0000

    clean up

--- ollama/serve.sh
+++ ollama/serve.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-port="${1:-11434}"
 folder="$(realpath $(dirname $0))"
-
 docker volume create ollama || exit $?
-if [ "$port" -le 0 ]; then
-    docker compose --file "$folder/docker-compose.yml" run --rm --detach ollama
-else
-    docker compose --file "$folder/docker-compose.yml" run --rm --detach --publish "$port:11434" ollama
-fi
+docker compose --file "$folder/docker-compose.yml" run --rm --detach --service-ports ollama
